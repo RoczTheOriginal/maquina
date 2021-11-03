57f14596e5bbf4d05e98c5590a203508564951ed
RoczTheOriginal <91512961+RoczTheOriginal@users.noreply.github.com>
2021-11-03 17:19:07 -0500

    Cambio total

---
+++ src/main/java/com/rentamaquina/maquina/app/crudRepository/AdminCrudRepository.java
@@ -0,0 +1,12 @@
+package com.rentamaquina.maquina.app.crudRepository;
+
+import com.rentamaquina.maquina.app.entities.Admin;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author @ROCKS
+ */
+public interface AdminCrudRepository extends CrudRepository<Admin,Integer> {
+    
+}
---
+++ src/main/java/com/rentamaquina/maquina/app/crudRepository/CategoryCrudRepository.java
@@ -0,0 +1,12 @@
+package com.rentamaquina.maquina.app.crudRepository;
+
+import com.rentamaquina.maquina.app.entities.Category;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author  ROCKS
+ */
+public interface CategoryCrudRepository extends CrudRepository<Category,Integer>{
+    
+}
