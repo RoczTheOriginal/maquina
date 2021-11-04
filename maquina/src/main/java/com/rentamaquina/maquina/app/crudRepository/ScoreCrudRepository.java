fdd5a300c64caeb9804af8d3119215e21978d819
RoczTheOriginal <91512961+RoczTheOriginal@users.noreply.github.com>
2021-11-03 21:37:03 -0500

    Add files via upload
    se suben nuevos archivos

---
+++ maquina/src/main/java/com/rentamaquina/maquina/app/crudRepository/ScoreCrudRepository.java
@@ -0,0 +1,12 @@
+package com.rentamaquina.maquina.app.crudRepository;
+
+import com.rentamaquina.maquina.app.entities.Score;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author  --------------
+ */
+public interface ScoreCrudRepository  extends CrudRepository<Score,Integer>{
+    
+}
